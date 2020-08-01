882db71c38928661844523132902fe3489db766a
EjazaliRezayi <67909901+EjazaliRezayi@users.noreply.github.com>
2020-08-01 19:41:18 -0400

    Almost finished adjustOrderSummary
    I'm not sure why it does not work
    Ill also have to adjust the styles

--- scripts/adjust-item-quantity.js
+++ scripts/adjust-item-quantity.js
@@ -1,14 +1,19 @@
+document.getElementById("icon").addEventListener("click", adjustOrderSummary());
 
 
-function adjustOrderSummary(name, priceUnit, newNumber) {
-    if(newNumber>0){
-        document.getElementById(name).style.display= "block";
-        document.getElementById(name).getElementById(price).innerHTML=(newNumber * priceUnit).toFixed(2) + " $";
-        document.getElementById(name).getElementById(number).innerHTML="x " + newNumber;
+function adjustOrderSummary() {
+    var items = document.getElementById("itemtable");
+    var order = document.getElementById("mytable");
+    for(var i=1;i<items.getElementsByTagName("tr").length;i++){
+        order.deleteRow(i);
     }
-    if(newNumber==0){
-        document.getElementById(name).innerHTML.style.display= "none";
+    for(var j=1;j<items.getElementsByTagName("tr").length;j++){
+        order.insertRow(j).insertCell(0).innerHTML = items.getElementsByTagName("p").innerHTML; // insert the name ex: Baguette
+        order.insertRow(j).insertCell(1).innerHTML = ("x "+items.rows[j].cells[2].innerHTML); // insert units ex: x 3
+        order.insertRow(j).insertCell(2).innerHTML = (items.rows[j].cells[1].innerHTML.substring(0,4)*items.rows[j].cells[2].innerHTML)+" $"; // insert price ex: 5.99 $
+        
     }
+
 }
 
 
