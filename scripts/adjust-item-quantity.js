65c7247fd5d75bbf2accdedeba9db43907987145
EjazaliRezayi <67909901+EjazaliRezayi@users.noreply.github.com>
2020-08-01 21:21:05 -0400

    er

--- scripts/adjust-item-quantity.js
+++ scripts/adjust-item-quantity.js
@@ -1,5 +1,7 @@
 document.getElementById("icon").addEventListener("click", adjustOrderSummary());
-
+window.onload = function(){
+    adjustOrderSummary();
+}
 
 function adjustOrderSummary() {
     var items = document.getElementById("itemtable");
@@ -7,11 +9,11 @@ function adjustOrderSummary() {
     for(var i=1;i<items.getElementsByTagName("tr").length;i++){
         order.deleteRow(i);
     }
-    for(var j=1;j<items.getElementsByTagName("tr").length;j++){
+    for(var j=1;j<items.rows.length;j++){
         order.insertRow(j).insertCell(0).innerHTML = items.getElementsByTagName("p").innerHTML; // insert the name ex: Baguette
         order.insertRow(j).insertCell(1).innerHTML = ("x "+items.rows[j].cells[2].innerHTML); // insert units ex: x 3
         order.insertRow(j).insertCell(2).innerHTML = (items.rows[j].cells[1].innerHTML.substring(0,4)*items.rows[j].cells[2].innerHTML)+" $"; // insert price ex: 5.99 $
-        
+
     }
 
 }
