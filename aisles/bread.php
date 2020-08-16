df7f08d6a675340fe6e9075df6492b253247fafa
georgesgrondin <68157087+georgesgrondin@users.noreply.github.com>
2020-08-16 14:23:03 -0400

    Convert homepage to .php, replace all references to .html with .php

--- aisles/bread.php
+++ aisles/bread.php
@@ -24,7 +24,7 @@
     <nav class="navbar navbar-expand-sm">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="../index.html">Home Page</a>
+                <a class="nav-link" href="../index.php">Home Page</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="../aisles/bread.php">Return to Aisle</a>
